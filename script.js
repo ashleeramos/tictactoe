dcb6a93398ddde30e7ee22722da3fa4379fefa90
ashleeramos <114506112+ashleeramos@users.noreply.github.com>
2022-11-21 18:17:06 +0000

    Did Stuff

--- script.js
+++ script.js
@@ -20,3 +20,57 @@ function showBoard() {
   }
   alert(board);
 }
+
+function newGame(){
+  let gameOver = false;
+  let goFirst = Math.floor(Math.random() * 2)
+  while (gameOver == false){
+    showBoard();
+    if (goFirst % 2 == 0){
+      if (cTurn() == true){
+        if (detectWin() == true){
+          winner = "cpu";
+          gameOver = true;
+        }
+      }
+      else {
+        winner = "nobody";
+      }
+    }
+    else {
+      if (uTurn() == true){
+        if (detectWin() == true){
+          winner = "user";
+          gameOver = true;
+        }
+      }
+      else {
+        winner = "nobody";
+      }
+    }
+  }
+  alert(winner + "wins")
+  playAgain();
+}
+
+function uTurn(){
+  let row = prompt("enter row");
+  let col = prompt("enter column");
+  alert (showBoard());
+  return;
+}
+
+function cTurn(){
+  if (defenseCheckR()) return true;
+  else if (defenseCheckC()) return true;
+  else if (defenseDiag()) return true;
+  else if (fillSpace()) return true;
+  else return false;
+}
+
+function detectWin(){
+  let row = 0;
+  let col = 0;
+  let xs = 0;
+  let os = 0;
+  for (let row = 0; row < 3; i++)
