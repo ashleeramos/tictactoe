2d3c049f15cc0087d6415bda972fe62029a4f875
ashlee ramos <114506112+ashleeramos@users.noreply.github.com>
2022-12-09 17:09:46 +0000

    getting there

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function makeBoard() {
     }
   }
   showBoard();
-  uTurn();
+  newGame();
 }
 
 function showBoard() {
@@ -29,37 +29,50 @@ function newGame() {
   let gameOver = false;
   let goFirst = Math.floor(Math.random() * 2);
   while (gameOver == false) {
-    showBoard();
     if (goFirst % 2 == 0) {
+      alert("cpu goes first");
+      cTurn();
       if (cTurn() == true) {
         if (detectWinR() == showBoard()) {
           winner = "cpu";
           gameOver = true;
         }
-      }
-      else {
-        winner = "nobody";
+        else if (detectWinC() == showBoard()) {
+          winner = "cpu";
+          gameOver = true;
+        }
+        else {
+          winner = "nobody";
+        }
       }
     }
     else {
+      alert("user goes first");
+      uTurn();
       if (uTurn() == true) {
         if (detectWinR() == true) {
           winner = "user";
           gameOver = true;
         }
-      }
-      else {
-        winner = "nobody";
+        else if (detectWinC() == true) {
+          winner = "user";
+          gameOver = true;
+        }
+        else {
+          winner = "nobody";
+        }
       }
     }
+    goFirst++;
+
   }
   alert(winner + "wins")
   playAgain();
 }
 
 function uTurn() {
-  let row = prompt("enter row");
-  let col = prompt("enter column");
+  let row = parseInt(prompt("enter row"));
+  let col = parseInt(prompt("enter column"));
   boardValues[row][col] = "x";
   alert(showBoard());
   return;
@@ -69,8 +82,21 @@ function cTurn() {
   if (defenseCheckR()) return true;
   else if (defenseCheckC()) return true;
   else if (defenseDiag()) return true;
-  else if (fillSpace()) return true;
+  else if (randomMove()) return true;
   else return false;
+  showBoard();
+}
+
+function defenseCheckR() {
+  return false;
+}
+
+function defenseCheckC() {
+  return false;
+}
+
+function defenseDiag() {
+  return false;
 }
 
 function detectWinR() {
@@ -89,7 +115,6 @@ function detectWinR() {
       winner = "o";
       return true;
     }
-    else return false;
     if (xs == 3) {
       winner = "x";
       return true;
@@ -114,8 +139,7 @@ function detectWinC() {
       winner = "o";
       return true;
     }
-    else return false;
-    if (os == 3) {
+    if (xs == 3) {
       winner = "o";
       return true;
     }
@@ -123,7 +147,7 @@ function detectWinC() {
   }
 }
 
-function defenseCheckR() {
+/* function defenseCheckR() {
   let os = 0;
   let xs = 0;
   for (let row = 0; row < 3; row++) {
@@ -168,12 +192,11 @@ function defenseCheckC() {
       }
     }
   }
-  return true;
+  return false;
 }
 
 function defenseDiag(slashIndex) {
   let os = 0;
-  let xs = 0;
   let flatBoard = boardValues.flat();
   boardValues.forEach(defenseDiag(fSlash));
   if (os > 1);
@@ -190,11 +213,6 @@ function defenseDiag(slashIndex) {
   else {
     slashIndex++;
     defenseDiag(slashIndex);
-  }
-  if (os == 2 && xs == 2) {
-    if (os + xs == 3) {
-      fillGapD(slashIndex);
-    }
   }
 }
 
@@ -217,11 +235,45 @@ function fillGapV(col) {
     }
     else {
       row++;
-      fillGapH();
+      fillGapV();
     }
   }
 }
 
 function fillGapD(slashIndex) {
+  let os = 0;
+  boardValues.forEach(defenseDiag(fSlash));
+  if (os > 1);
+  os = 0;
+  boardValues.forEach(defenseDiag(bSlash));
+  if (os > 1);
+  os = 0;
+  if (flatBoard[slashIndex] == "_") {
+    flatBoard[slashIndex] = "o";
+  }
+  else {
+    row++
+    fillGapD(slashIndex);
+  }
+} */
 
+function randomMove() {
+  let blanks = [];
+  for (let col = 0; col < 3; col++) {
+    for (let row = 0; row < 3; row++) {
+      if (boardValues[row][col] == "_") {
+        blanks.push([row, col]);
+      }
+    }
+  }
+  if (blanks == 0) {
+    return false;
+  }
+  else {
+    let which = Math.floor(Math.random() * blanks.length);
+    let row = blanks[which][0];
+    let col = blanks[which][1];
+    boardValues[row][col] = "o";
+    return true;
+  }
 }
