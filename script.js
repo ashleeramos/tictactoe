26db3554d5301687c7bce4ce5f74e5fa7ff39955
ashlee ramos <114506112+ashleeramos@users.noreply.github.com>
2023-01-05 14:24:39 +0000

    refactor detectwins, problem with cturn

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var boardValues = [];
-var winner = "";
+var winner = "nobody";
 var fSlash = [2, 4, 6];
 var bSlash = [0, 4, 8];
-var boardDict = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
+var boardDict = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]];
+var gameOver = false;
 
 function makeBoard() {
   for (let row = 0; row < 3; row++) {
@@ -26,62 +27,58 @@ function showBoard() {
 }
 
 function newGame() {
-  let gameOver = false;
-  let goFirst = Math.floor(Math.random() * 2);
+  let turn = 0;
+  let go = Math.floor(Math.random() * 2);
   while (gameOver == false) {
     showBoard();
-    if (goFirst % 2 == 0) {
+    if (go % 2 == 0) {
+      turn++;
       if (cTurn() == true) {
-        if (detectWinR() == true) {
-          winner = "cpu";
-          gameOver = true;
-        }
-        else if (detectWinC() == true) {
-          winner = "cpu";
-          gameOver = true;
-        }
-        else if (detectWinD(fSlash) == true) {
-          winner = "cpu";
-          gameOver = true;
-        }
-        else if (detectWinD(bSlash) == true) {
-          winner = "cpu";
-          gameOver = true;
-        }
-        else {
-          winner = "nobody";
-        }
+        detectWin("cpu");
       }
+      go++;
+      alert(turn + " cTurn");
     }
-    else if (goFirst % 2 > 0) {
+    else if (go % 2 > 0) {
+      turn++;
       if (uTurn() == true) {
-        if (detectWinR() == true) {
-          winner = "user";
-          gameOver = true;
-        }
-        else if (detectWinC() == true) {
-          winner = "user";
-          gameOver = true;
-        }
-        else if (detectWinD(fSlash) == true) {
-          winner = "user";
-          gameOver = true;
-        }
-        else if (detectWinD(bSlash) == true) {
-          winner = "user";
-          gameOver = true;
-        }
-        else {
-          winner = "nobody";
-          break;
-        }
+        detectWin("user");
       }
-      goFirst++;
+      go++;
+      alert(turn + " uTurn");
     }
-    alert(winner + " wins")
-    // playAgain();
+    // new turn
   }
+  alert(winner + " wins");
+  // playAgain();
 }
+
+function detectWin(player) {
+  if (detectWinR() == true) {
+    winner = player;
+    gameOver = true;
+  }
+  else if (detectWinC() == true) {
+    winner = player;
+    gameOver = true;
+  }
+  else if (detectWinD(fSlash) == true) {
+    winner = player;
+    // alert("fSlash won");
+    gameOver = true;
+  }
+  else if (detectWinD(bSlash) == true) {
+    winner = player;
+    // alert("bSlash won");
+    gameOver = true;
+  }
+  else {
+    winner = "problem";
+  }
+  return gameOver;
+}
+
+
 
 function uTurn() {
   let row = parseInt(prompt("enter row"));
@@ -103,22 +100,12 @@ function cTurn() {
   else if (randomMove()) return true;
 }
 
-/* function defenseCheckR() {
-  return false;
-} */
-
-/* function defenseCheckC() {
-  return false;
-} */
-
-/* function defenseDiag() {
-  return false;
-} */
-
 function detectWinR() {
   let os = 0;
   let xs = 0;
   for (let row = 0; row < 3; row++) {
+    os = 0;
+    xs = 0;
     for (let col = 0; col < 3; col++) {
       if (boardValues[row][col] == "o") {
         os++;
@@ -142,25 +129,30 @@ function detectWinR() {
 function detectWinC() {
   let os = 0;
   let xs = 0;
+  let winFlag = false;
   for (let col = 0; col < 3; col++) {
+    os = 0;
+    xs = 0;
     for (let row = 0; row < 3; row++) {
       if (boardValues[row][col] == "o") {
         os++;
       }
-      if (boardValues[row][col] == "x") {
+      else if (boardValues[row][col] == "x") {
         xs++;
       }
     }
     if (os == 3) {
       // winner = "o";
-      return true;
+      alert("winner column");
+      winFlag = true;
     }
     else if (xs == 3) {
       // winner = "x";
-      return true;
+      alert("winner column");
+      winFlag = true;
     }
-    else return false;
   }
+  return winFlag;
 }
 
 function defenseCheckR() {
@@ -215,6 +207,7 @@ function defenseCheckC() {
 
 function defenseDiag(items) {
   let flatBoard = boardValues.flat();
+  let os = 0;
   let xs = 0;
   let open = -1;
   items.forEach(defenseDiagForEach);
@@ -228,32 +221,39 @@ function defenseDiag(items) {
   function defenseDiagForEach(item, index, arr) {
     if (flatBoard[item] == "x") {
       xs++;
-      console.log("x at " + boardDict[item] + " at " + index + " in " + arr);
+      // console.log("x at " + boardDict[item] + " at " + index + " in " + arr);
+    }
+    else if (flatBoard[item] == "o") {
+      os++;
     }
     else if (flatBoard[item] == "_") {
       open = item;
-      console.log("_ at " + boardDict[item] + " at " + index + " in " + arr);
+      // console.log("_ at " + boardDict[item] + " at " + index + " in " + arr);
     }
   }
   function fillGapD(item) {
     let toFill = boardDict[item];
     boardValues[toFill[0]][toFill[1]] = "o";
-    console.log("o at " + toFill);
+    // console.log("o at " + toFill);
   }
 }
 
 function detectWinD(items) {
   let flatBoard = boardValues.flat();
   let os = 0;
+  let xs = 0;
   items.forEach(detectWinDForEach);
   // bSlash && fSlash
-  if (os == 3) {
+  if (os == 3 || xs == 3) {
     return true;
   }
   else return false;
   function detectWinDForEach(item) {
     if (flatBoard[item] == "o") {
       os++;
+    }
+    else if (flatBoard[item] == "x") {
+      xs++;
     }
   }
 }
@@ -270,7 +270,6 @@ function fillGapH(row) {
   }
 }
 
-
 function fillGapV(col) {
   let row = 0;
   while (row < 3) {
@@ -282,7 +281,6 @@ function fillGapV(col) {
     }
   }
 }
-
 
 function randomMove() {
   let blanks = [];
@@ -304,4 +302,12 @@ function randomMove() {
     boardValues[row][col] = "o";
     return true;
   }
+}
+
+function playAgain() {
+  let again = prompt("would you like to play again (yes or no)?");
+  if (again = "yes") {
+    newGame();
+  }
+  else return "thanks for playing tic-tac-toe!";
 }
